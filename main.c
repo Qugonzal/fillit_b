0cf714d5f87c46ccabda50c2144d96e6fa00745b
Quentin GONZALEZ <quegonza@e1r11p5.42.fr>
2019-08-26 19:27:30 +0200

    lil_maj

--- main.c
+++ main.c
@@ -6,11 +6,34 @@
 /*   By: quegonza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 18:00:02 by quegonza          #+#    #+#             */
-/*   Updated: 2019/08/26 18:24:18 by quegonza         ###   ########.fr       */
+/*   Updated: 2019/08/26 19:26:05 by quegonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libfil.h"
+
+char	**ft_reader(char *buf)
+{
+	int			fd;
+	int			i;
+	char		buffer[547];
+	char		*buffer_copy;
+
+	fd = open(file, O_RDONLY);
+	ft_bzero(buffer, sizeof(buffer));
+	if (!(i = read(fd, buffer, 546)) || i == -1 || i > 546 || (close(fd) == -1))
+		ft_display_error(2);
+	
+}
+
+void	ft_display_error(int message)
+{
+	if (message == 1)
+		ft_putendl("usage: input file");
+	else if (message == 2)
+		ft_putendl("error");
+	exit(0);
+}
 
 int		main(int ac, char **av)
 {
